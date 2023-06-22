2bd56a45868bb92dac3ed09fd5fb4cfa3f4b1ba3
Olga Boiko <Itnumber1@ukr.net>
2023-06-22 22:35:31 +0300

    add task1 without styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import PropTypes from 'prop-types'
+import { Profile } from "./Profile/Profile";
+
+import user from '../user.json'
 export const App = () => {
   return (
     <div
@@ -10,7 +14,29 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
     </div>
   );
 };
+
+App.propTypes={
+  obj: PropTypes.objectOf(
+    PropTypes.exact({
+      username: PropTypes.string.isRequired,
+tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: {
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired
+    }
+  })
+  )
+}
