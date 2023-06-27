786792a559f2149364c3bb8659cdee8da61c493d
Olga Boiko <Itnumber1@ukr.net>
2023-06-28 00:34:56 +0300

    add styles task2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,4 +1,4 @@
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import css from './Statistics.module.css';
 export const Statistics = ({ stats, title }) => {
   return (
@@ -8,7 +8,7 @@ export const Statistics = ({ stats, title }) => {
         {stats.map(stat => (
           <li key={stat.id} className={css.item}>
             <span className={css.label}>{stat.label}</span>
-            <span className={css.percentage}>{stat.percentage}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
           </li>
         ))}
       </ul>
@@ -16,12 +16,18 @@ export const Statistics = ({ stats, title }) => {
   );
 };
 
-// Statistics.propTypes ={
-//     title: PropTypes.string,
-//     stats: PropTypes.arrayOf(PropTypes.objectOf(
-//       id: PropTypes.string.isRequired,
-//       label: PropTypes.string.isRequired,
-//       percentage: PropTypes.number.isRequired
-//       )
-//       )
+Statistics.propTypes ={
+    title: PropTypes.string,
+    stats: PropTypes.objectOf(
+      PropTypes.string,
+     PropTypes.string,
+     PropTypes.number
+      )
+      
+}
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
 // }
